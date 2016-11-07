ed8340f6dd04ffdd78be77edad2f4bfa042a414f
Moritz Kammerer <moritz.kammerer@qaware.de>
2016-11-07 04:35:57 +0100

    Added more comments.

--- src/test/kotlin/de/mkammerer/Crypto.kt
+++ src/test/kotlin/de/mkammerer/Crypto.kt
@@ -6,23 +6,32 @@ import org.junit.Test
 
 class CBCTests {
     @Test fun testCbcAndHmac() {
+        // Generate two keys: one for CBC, one for the HMAC
         val cbcKey = generateKey(256)
         val hmacKey = generateKey(256)
+
         val plaintext = "This is the CBC test"
 
+        // Encrypt the plaintext using AES CBC, the IV is generated automatically
         val ciphertext = encryptCbc(plaintext.toByteArray(), cbcKey)
         val hmac = createHmac(ciphertext.iv + ciphertext.ciphertext, hmacKey)
 
+        // Now send the IV, the ciphertext and the HMAC over wire, or store it somewhere. It doesn't contain any secret information.
+
+        // Before decrypting, check the HMAC. If it doesn't match, someone has tampered the data!
         if (!checkHmac(ciphertext.iv + ciphertext.ciphertext, hmacKey, hmac)) throw IllegalStateException("HMAC failed")
+        // Decrypt the ciphertext. The decrypt message uses the IV which is stored in the ciphertext object
         val decrypted = String(decryptCbc(ciphertext, cbcKey), Charsets.UTF_8)
 
         Assert.assertThat(decrypted, CoreMatchers.equalTo(plaintext))
     }
 
     @Test fun testGcm() {
+        // GCM only needs one key. If you can use GCM, prefer that over CBC + HMAC
         val key = generateKey(256)
         val plaintext = "This is the GCM test"
 
+        // GCM uses a nonce. The encrypt message uses a random nonce. NEVER REUSE A NONCE!
         val ciphertext = encryptGcm(plaintext.toByteArray(), key)
 
         val decrypted = String(decryptGcm(ciphertext, key), Charsets.UTF_8)
